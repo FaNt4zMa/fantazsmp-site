ce49271d9e57e362e677920a7bafbcd20c597123
BuildTools <unconfigured@null.spigotmc.org>
2025-09-09 02:52:19 -0400

    Initial commit of local files

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  mapUrl: "https://bluecolored.de/bluemap/#overworld:-799:41:428:198:0:0:0:0:perspective",
+  baseUrl: "",   // Crafty base URL - Handled by the proxy, so leave empty
+  serverId: "0bfac3d0-c09b-4580-90c5-08df6c68f8d8",   // Crafty's server ID/UUID
+  pollMs: 5000
+};

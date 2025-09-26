8ceb13dc061cce0ef068d674be88a9da305898dc
Fantaz <nathanyel916d@gmail.com>
2025-09-25 21:17:25 -0400

    Add comment for Bluemap URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  mapUrl: "https://bluecolored.de/bluemap/#overworld:-799:41:428:198:0:0:0:0:perspective",
+  mapUrl: "https://bluecolored.de/bluemap/#overworld:-799:41:428:198:0:0:0:0:perspective", // Bluemap URL
   baseUrl: "",   // Crafty base URL - Handled by the proxy, so leave empty
   serverId: "0bfac3d0-c09b-4580-90c5-08df6c68f8d8",   // Crafty's server ID/UUID
   pollMs: 5000
